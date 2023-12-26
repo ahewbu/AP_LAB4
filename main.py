354e308541848dc9e3a64e87411a012de128bcd6
ahewbu <shlnsn@outlook.com>
2023-12-26 19:39:06 +0400

    added functions of text's preprocessing

--- main.py
+++ main.py
@@ -3,10 +3,59 @@ import os
 import nltk
 import matplotlib.pyplot as plt
 import re
+import nltk
+from nltk.corpus import stopwords
+from pymorphy2 import MorphAnalyzer
+from pymystem3 import Mystem
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from collections import Counter
 
+mystem = Mystem()
+stop_words = set(stopwords.words('russian'))
+patterns = "[A-Za-z0-9!#$%&'()*+,./:;<=>?@[\]^_`{|}~—\"\-]+"
+morph = MorphAnalyzer()
+
+
+def preprocess_text(text: str):
+    """Function gets text and removes stopwords and patterns
+
+    Args:
+        text (str): text for preprocess 
+
+    Returns:
+        List[str]: preprocessed text
+    """
+    words = re.sub(patterns, ' ', text)
+    tokens = []
+    for token in words.split():
+        if token and token not in stop_words:
+            token = token.strip()
+            token = morph.normal_forms(token)[0]
+            tokens.append(token)
+    if len(tokens) > 2:
+        return tokens
+    text = " ".join(tokens)
+    tokens = word_tokenize(text)
+    return tokens
+
+def preprocess_text_only_A(text: str) :
+    """Function gets text, lemmatize them and removes all word without adjective and adverb
+
+    Args:
+        text (str): text for preprocess
+
+    Returns:
+        List[str]: preprocessed text
+    """
+    tokens = mystem.lemmatize(str(text))
+    tokens = [token for token in tokens if token not in stop_words]
+    text = " ".join(tokens)
+    words = nltk.word_tokenize(text)
+    functors_pos = {'A=m', 'ADV'}
+    res = [word for word, pos in nltk.pos_tag(words, lang='rus')
+           if pos not in functors_pos]
+    return res
 
 def count_words(text : str):
     """Function counts information about the number of words in a cell
@@ -82,7 +131,9 @@ def plot_word_histogram(dataframe : pd.DataFrame, label : str):
     else:
         text_block = " ".join(dataframe[dataframe['Количество звёзд'] == label]['Текст рецензии'])
 
-    tokens = word_tokenize(text_block)
+    
+    tokens = preprocess_text_only_A(preprocess_text(text_block))
+    
 
     lemmatizer = WordNetLemmatizer()
     lemmatized_tokens = [lemmatizer.lemmatize(token.lower()) for token in tokens if token.isalnum()]
